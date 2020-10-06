3b8f234ba461f90e6c7d09d40679f46e370c097d
Henrique Dias <mrhdias@gmail.com>
2020-10-06 11:35:53 +0100

    Update asyncfcgiserver.nim

--- fastkiss/asyncfcgiserver.nim
+++ fastkiss/asyncfcgiserver.nim
@@ -648,8 +648,8 @@ proc newAsyncFCGIServer*(): AsyncFCGIServer =
     for add in fwsa.split(','):
       result.allowedIps.add(add.strip())
       
- when not defined(testing) and isMainModule:
-  proc main =
+when not defined(testing) and isMainModule:
+  proc main() =
     let app = newAsyncFCGIServer()
     app.get("/", proc (req: Request) {.async.} =
       await req.response("Hello World!")
