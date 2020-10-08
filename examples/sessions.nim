323ae29fb7308abbe3abe65d73661f07dc9b87c9
Henrique Dias <mrhdias@gmail.com>
2020-10-08 19:19:39 +0100

    Create sessions.nim

---
+++ examples/sessions.nim
@@ -0,0 +1,72 @@
+#
+# nimble install https://github.com/mrhdias/fastkiss
+# nim c -r sessions.nim 
+# http://example:8080/sessions
+#
+import fastkiss
+import fastkiss/asyncsessions
+import re
+from strutils import `%`
+from strformat import `&`
+from cookies import parseCookies
+import sugar
+
+proc showSessionPage(
+  req: Request,
+  sessions: AsyncSessions) {.async.} =
+
+  var id = ""
+  if req.headers.hasKey("cookie"):
+    let cookies = parseCookies(req.headers["cookie"])
+    if cookies.hasKey("session"):
+      id = cookies["session"]
+
+    if id != "" and (id in sessions.pool):
+      if (let session = sessions.get_session(id); session) != nil:
+        await req.response(
+          &"""Hello User {session.map["username"]} Again :-) {id}""",
+          newHttpHeaders([
+            ("content-type", "text/plain;charset=utf-8")
+          ]),
+          200
+        )
+        # sessions.delSession(id)
+      else:
+        await req.response("Session Error!")
+
+      return
+
+  proc timeoutSession(id: string) {.async.} =
+    echo "expired session: ", id
+
+  var session = sessions.setSession()
+  session.map["username"] = "Kiss"
+  session.callback = timeoutSession
+
+  await req.response(
+    "New Session",
+    newHttpHeaders([
+      ("Set-Cookie", &"session={session.id}"),
+      ("content-type", "text/html;charset=utf-8")
+    ]),
+    200
+  )
+
+
+proc main() =
+
+  let sessions = newAsyncSessions(
+    sleepTime=1000,
+    sessionTimeout=30
+  )
+
+  let app = newAsyncFCGIServer()
+  app.config.port = 9000 # optional if default port
+
+  # app.get("/sessions", proc (req: Request): Future[void] = showSessionPage(req, sessions))
+  # with sugar module
+  app.get("/sessions", (req: Request) => showSessionPage(req, sessions))
+
+  app.run()
+
+main()
