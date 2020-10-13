8709937860ef71675bf4111fe5e0914ca334a845
Henrique Dias <mrhdias@gmail.com>
2020-10-13 19:50:23 +0100

    Update and rename server.nim to app.nim

---
+++ tests/app.nim
@@ -0,0 +1,30 @@
+import asyncdispatch
+import ../fastkiss
+
+var stop = false
+
+proc shutdown(app: AsyncFCGIServer) {.async.} =
+  while true:
+    if stop:
+      app.close()
+      echo "App test shutdown completed."
+      quit(QuitSuccess)
+
+    await sleepAsync(2000)
+    echo "Start the app test shutdown ..."
+    stop = true
+
+
+proc main() =
+  let app = newAsyncFCGIServer()
+  app.config.port = 9000 # optional if default port
+
+  asyncCheck app.shutdown()
+
+  app.get("/", proc (req: Request) {.async.} =
+    await req.response("Hello World!")
+  )
+
+  app.run()
+
+main()
